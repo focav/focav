a36de012786a5b6d84d940578a0ff6ad3ea69c6d
focav <84325985+focav@users.noreply.github.com>
2021-05-17 13:44:34 +0100

    Add files via upload

---
+++ cible.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>FETE DU NGUON</title>
+        
+    </head>
+    <body>
+        
+     <p>Bonjour à toi <?php echo $_POST['nom']; ?> un message vient dêtre envoyé à ton adresse email suivante: <?php echo $_POST['Appréciation']; ?></p>
+     
+    </body>
+</html>
